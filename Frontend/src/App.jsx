d3d4c6e9eb9cc6daa682d2ea5e854c4bd5765bf7
keshav Garg <garg68860@gmail.com>
2024-12-21 00:14:06 +0530

    start making my shortify project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './index.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <div className="flex justify-center items-center max-h-[100%]">
+          <h1 className="text-yellow-400 ">hello! I am keshav garg...</h1>
+     </div>
+
+    </>
+  )
+}
+
+export default App
